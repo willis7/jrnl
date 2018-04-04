e2d2f729d5d688fae2854097f39c9e163e9e2029
Sion Williams <sion@byteshifter.io>
2018-04-04 21:42:32 +0100

    Add client initialisation to rootCmd init()
    Simplify main by initialising the db client as a package level variable
    inside the rootCmd's init method.

--- cmd/list.go
+++ cmd/list.go
@@ -2,32 +2,30 @@ package cmd
 
 import (
 	"fmt"
-	"os"
 
 	"github.com/spf13/cobra"
-	"github.com/willis7/jrnl/db"
 )
 
-func CreateListCmd(client db.IClient) {
-	var listCmd = &cobra.Command{
-		Use:   "list",
-		Short: "Lists all of your entries.",
-		Run: func(cmd *cobra.Command, args []string) {
-			entries, err := client.AllEntries()
-			if err != nil {
-				fmt.Println("failed to retrieve entries:", err)
-				os.Exit(1)
-			}
-			if len(entries) == 0 {
-				fmt.Println("you have no entries")
-				return
-			}
-			fmt.Println("Here's your jrnl entries:")
-			for i, entry := range entries {
-				fmt.Printf("%d. %s %s\n", i+1, entry.Date, entry.Text)
-			}
-		},
-	}
+var listCmd = &cobra.Command{
+	Use:   "list",
+	Short: "Lists all of your entries.",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		entries, err := client.AllEntries()
+		if err != nil {
+			return fmt.Errorf("failed to retrieve entries: %s", err)
+		}
+		if len(entries) == 0 {
+			fmt.Println("you have no entries")
+			return nil
+		}
+		fmt.Println("Here's your jrnl entries:")
+		for i, entry := range entries {
+			fmt.Printf("%d. %s %s\n", i+1, entry.Date, entry.Text)
+		}
+		return nil
+	},
+}
 
+func init() {
 	RootCmd.AddCommand(listCmd)
 }
--- cmd/remove.go
+++ cmd/remove.go
@@ -3,29 +3,26 @@ package cmd
 import (
 	"fmt"
 
-	"github.com/willis7/jrnl/db"
-
 	"github.com/spf13/cobra"
 )
 
-// CreateRemoveCmd closes over a client and adds a remove command
-func CreateRemoveCmd(client db.IClient) {
-	// removeCmd represents the remove command
-	var removeCmd = &cobra.Command{
-		Use:        "remove",
-		Short:      "Removes a journal entry.",
-		SuggestFor: []string{"delete", "rm"},
-		Args:       cobra.MinimumNArgs(1),
-		Run: func(cmd *cobra.Command, args []string) {
-			for _, date := range args {
-				err := client.DeleteEntry(date)
-				if err != nil {
-					fmt.Printf("failed to delete \"%s\". error: %s", date, err)
-				} else {
-					fmt.Printf("deleted \"%s\"\n", date)
-				}
+var removeCmd = &cobra.Command{
+	Use:        "remove",
+	Short:      "Removes a journal entry.",
+	SuggestFor: []string{"delete", "rm"},
+	Args:       cobra.MinimumNArgs(1),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		for _, date := range args {
+			err := client.DeleteEntry(date)
+			if err != nil {
+				return fmt.Errorf("failed to delete \"%s\". error: %s", date, err)
 			}
-		},
-	}
+			fmt.Printf("deleted \"%s\"\n", date)
+		}
+		return nil
+	},
+}
+
+func init() {
 	RootCmd.AddCommand(removeCmd)
 }
